1cadc85b8bf66a5bb1e6bf300f67bfe73340ca53
Davit Ivanyan <davit.ivanyan@teamviewer.com>
2023-02-22 15:25:09 +0000

    Add support-email and style changes

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
-setup(name='teamviewer_iot_python_sdk',
+setup(name='teamviewer_iot_mqtt_sdk',
       version='1.1.0',
       description='Iot client to put data in TeamViewer Iot.',
       url='',
       author='TeamViewer',
-      author_email='@TeamViewer.com',
+      author_email='iot-support@teamviewer-iot.com',
       license='MIT',
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
-      packages=['TeamViewerIoTPythonSDK', 'docs', 'examples'],
-      package_data={'docs': ['html/*.html', 'html/*.js', 'html/*.inv', 'html/_static/*']},
+      packages=['TeamViewerIoTPythonSDK'],
       include_package_data=True,
       install_requires=[
           'setuptools>=18.5',
@@ -17,4 +16,3 @@ setup(name='teamviewer_iot_python_sdk',
           'paho-mqtt==1.3.1'
       ],
       zip_safe=False)
-
